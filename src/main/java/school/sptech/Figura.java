924f963c38c7557b6d3dd67eb1429b128a3cd9d3
Lauriano29 <cristhian29192003@gmail.com>
2024-11-07 18:43:54 -0300

    realizado

--- src/main/java/school/sptech/Figura.java
+++ src/main/java/school/sptech/Figura.java
@@ -1,4 +1,42 @@
 package school.sptech;
 
-public class Figura {
+public abstract class Figura {
+
+   private String cor;
+   private Integer espessura;
+
+   public Figura(){
+
+   }
+
+    public Figura(String cor, Integer espressura) {
+        this.cor = cor;
+        this.espessura = espressura;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getEspessura() {
+        return espessura;
+    }
+
+    public void setEspessura(Integer espessura) {
+        this.espessura = espessura;
+    }
+
+    @Override
+    public String toString() {
+        return "Figura{" +
+                "cor='" + cor + '\'' +
+                ", espressura=" + espessura +
+                '}';
+    }
+
+    public abstract Double calcularArea();
 }
--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,50 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+
+    private List<Figura> figuras =new ArrayList<>();
+
+    public List<Figura> getFiguras() {
+        return figuras;
+    }
+
+    public void setFiguras(List<Figura> figuras) {
+        figuras = figuras;
+    }
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double valor_total = 0.0;
+        for(Figura list:figuras){
+            valor_total+=list.calcularArea();
+        }
+        return valor_total;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura>Maior20= new ArrayList<>();
+        for(Figura list:figuras){
+            if (list.calcularArea()>20) {
+                Maior20.add(list);
+            }
+        }
+        return Maior20;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura>Qudrados= new ArrayList<>();
+        for(Figura list:figuras) {
+            if (list instanceof Quadrado) {
+                Qudrados.add(list);
+            }
+        }
+            return Qudrados;
+    }
+
 }
